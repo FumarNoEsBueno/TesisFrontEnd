a694b2f42d42d470a93d4f083af8b1ebbd3f357b
FumarEsMalo <marcelo.murillo.99@hotmail.com>
2024-04-10 13:03:28 -0400

    commit

---
+++ src/components/display-disco-duro/display-disco-duro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-disco-duro',
+  standalone: true,
+  imports: [],
+  templateUrl: './display-disco-duro.component.html',
+  styleUrl: './display-disco-duro.component.less'
+})
+export class DisplayDiscoDuroComponent {
+
+}
