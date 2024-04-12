afa78e95fa75279a70532eb1af87babc895666fd
mercelo <marcelo.murillo@engagemeapps.com>
2024-04-12 17:05:55 -0400

    Cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from '../components/nav-bar/nav-bar.component';
 import { DisplayDiscoDuroComponent } from '../components/display-disco-duro/display-disco-duro.component';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, DisplayDiscoDuroComponent, RouterModule],
+  imports: [ButtonModule, RouterOutlet, NavBarComponent, DisplayDiscoDuroComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
--- src/app/views/mostrador-disco-duro/mostrador-disco-duro.component.ts
+++ src/app/views/mostrador-disco-duro/mostrador-disco-duro.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { StyleClassModule } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-mostrador-disco-duro',
   standalone: true,
-  imports: [CardModule, ButtonModule],
+  imports: [CardModule, ButtonModule, StyleClassModule],
   templateUrl: './mostrador-disco-duro.component.html',
   styleUrl: './mostrador-disco-duro.component.less'
 })
