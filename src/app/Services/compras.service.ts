e903075ae5e1041de67fb9ce7670eeaea2ed5a30
FumarEsMalo <marcelo.murillo.99@hotmail.com>
2024-04-16 19:29:57 -0400

    commit

---
+++ src/app/Services/compras.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComprasService {
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  apiCall(){
+    return this.http.get('http://127.0.0.1:8000/api/test');
+  }
+}
